ef05e0951712cd6f6499560f69ef60edc6f5856c
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-10-23 14:39:30 +0200

    Imprementacja #k

--- lab3/main.c
+++ lab3/main.c
@@ -3,11 +3,21 @@
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
+		if((format_string[i] == '#') && (format_string[i+1] == 'k')) {
 			printf("%s",param);
-		}else
-			putchar(format_string[i]);
+			
+			for (int j = 0; j < strlen(format_string); j++) {
+				if ((format_string[j] >= 'a') && (format_string[j] <= 'z')){
+					format_string[j] += 32;
+				}
+				else if ((format_string[j] >= 'A') && (format_string[j] <= 'Z')){
+					format_string[j] -= 32;
+				}
+
+			}
+			i++;
+	}else
+		putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
