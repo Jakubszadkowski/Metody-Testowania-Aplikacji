72e1cfa0d75caefd6cfd91461a3d8a987e2ebfa7
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-11-06 15:56:20 +0100

    Dodano obsluge zer.

--- lab4/main.c
+++ lab4/main.c
@@ -2,6 +2,15 @@
 #include <string.h>
 #include <ctype.h>
 
+int ileZer(char* temp,int size){
+	int zera=-1;
+	for (int i = 0 ; i < size ; i++)
+		{
+			if(temp[i]=='0')
+				zera++;
+		}
+	return zera;
+}
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'g')) {
@@ -19,7 +28,12 @@ int my_printf(char *format_string, char *param){
 				}
 			
 			}
+			int zera = ileZer(temp,size);
 			for(int j = size-1 ; j >= 0 ; j--){
+				if(zera==j)
+					break;
+				if(temp[j]=='0')
+					zera--;
 				putchar(temp[j]);
 			}
 			break;
