849001ba2e890dba8dac04dfe790053c5e6f8ec3
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-11-06 14:48:01 +0100

    Implementacja #g

--- lab4/main.c
+++ lab4/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
+		if((format_string[i] == '#') && (format_string[i+1] == 'g')) {
 			printf("%s",param);
-		}else
+			char temp[1024];
+			int size=0;
+			putchar(' ');
+			for (int j = i+3; j < strlen(format_string); j++) {
+				if (isdigit(format_string[j])){
+					temp[size]=format_string[j];
+					size++;
+				}
+				else{
+					putchar('%');
+					break;
+				}
+			
+			}
+			for(int j = size-1 ; j >= 0 ; j--){
+				putchar(temp[j]);
+			}
+
+			break;
+
+	}else
 			putchar(format_string[i]);
 	}
 	puts("");
