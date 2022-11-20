2b181b17187d9f6252968910450bad678699c419
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-11-20 15:02:42 +0100

    Znajdowanie #Xd

--- lab5/main.c
+++ lab5/main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
+
+int ileZer(char* temp,int size){
+	int zera=-1;
+	for (int i = 0 ; i < size ; i++)
+		{
+			if(temp[i]=='0')
+				zera++;
+		}
+	return zera;
+}
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		printf("ata");
-		if((format_string[i] == '#') && ((format_string[i+2] == 'g'))){
-			i++;
-			int atrybut = atoi(format_string+i);
-			printf("atrybut = %d",atrybut);
+		if((format_string[i] == '#') && ((format_string[i+2] == 'g')||(format_string[i+3] == 'g'))) {
+			int atrybut = atoi(format_string+(i+1));
+			printf("%s",param);
 
-			// printf("%s",param);
-		}else{
-			// putchar(format_string[i]);
+			
+			
+
+	}else
+			putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
-	}
 }
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
