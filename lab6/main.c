3d27293733d2068b5ccae1e5b5872d901b2eade0
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-12-11 14:43:05 +0100

    Added function from previous lab.

--- lab6/main.c
+++ lab6/main.c
@@ -1,18 +1,76 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+int getInt(char temp){
+	switch (temp)
+	{
+	case '0':
+		return 0;
+		break;
+	case '1':
+		return 1;
+		break;
+	case '2':
+		return 2;
+		break;
+	case '3':
+		return 3;
+		break;
+	case '4':
+		return 4;
+		break;
+	case '5':
+		return 5;
+		break;
+	case '6':
+		return 6;
+		break;
+	case '7':
+		return 7;
+		break;
+	case '8':
+		return 8;
+		break;
+	case '9':
+		return 9;
+		break;							
+	default:
+		break;
+	}
+	return 0;
+}
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
+		if((format_string[i] == '#') && (format_string[i+1] == '.')&&(format_string[i+3] == 'g')) {
+			int atrybut = atoi(format_string+(i+1));
 			printf("%s",param);
-		}else
+			int dlugosc = 3;
+			int flag=1;
+			if(format_string[i+3]!=' ')
+				dlugosc++;
+			putchar(' ');
+			for(int j = i+dlugosc+1;j<strlen(format_string);j++){
+				if(atrybut>strlen(format_string)-(j)&&flag==1){
+					flag=2;
+					for(int k = 0;k< atrybut-(strlen(format_string)-j);k++){
+						putchar('0');
+					}
+				}
+				flag=2;
+			int liczba = getInt(format_string[j]);
+			liczba=(liczba*9+1)/10;	
+			putchar(liczba);
+			}
+			break;
+			
+
+	}else
 			putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
 }
-
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
 	while(gets(buf)){
