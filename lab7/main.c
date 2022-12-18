995ba9054e44da5817884bd894e8c1d7e15afad3
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-12-18 15:11:41 +0100

    Removed all blank spaces and adjusted code.

--- lab7/main.c
+++ lab7/main.c
@@ -3,49 +3,47 @@
 #include <stdlib.h>
 #include <math.h>
 
-void hex(long n)
-{
+void hex(long n){
 	if(n>0){
-		hex(n/16); 
-	if(n%16>9)
-	{
-		switch(n%16){
-			case 10:
-				putchar('g'); 
-				break;
-			case 11:
-				putchar('h');
-				break;
-			case 12:
-				putchar('i');
-				break;
-			case 13:
-				putchar('j');
-				break;
-			case 14:
-				putchar('k');
-				break;
-			case 15:
-				putchar('l');
-				break;
+		hex(n/16);
+		if(n%16>9){
+			switch(n%16){
+				case 10:
+					putchar('g');
+					break;
+				case 11:
+					putchar('h');
+					break;
+				case 12:
+					putchar('i');
+					break;
+				case 13:
+					putchar('j');
+					break;
+				case 14:
+					putchar('k');
+					break;
+				case 15:
+					putchar('l');
+					break;
+			}
+		}
+		else{
+			char temp = (n%16)+'0';
+			putchar(temp);
 		}
 	}
-	else{
-		char temp = (n%16)+'0';
-		putchar(temp);	
-	}
-}
 }
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'j')) {
+		if((format_string[i] == '#') && (format_string[i+1] == 'j')){
 			printf("%s",param);
 			putchar(' ');
 			long numb = atoi(format_string+(i+2));
 			hex(numb);
 			break;
-	}else
-			putchar(format_string[i]);
+		}else
+				putchar(format_string[i]);
 	}
 	puts("");
 	return 0;
