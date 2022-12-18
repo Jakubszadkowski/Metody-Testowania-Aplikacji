c1d8a0fb22c9f9bf2e0036843f1794f857db5418
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2022-12-18 14:46:57 +0100

    Added 10 to 16

--- lab7/main.c
+++ lab7/main.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
+void hex(long n)
+{
+	if(n>0){
+		hex(n/16); 
+	if(n%16>9)
+	{
+		switch(n%16){
+			case 10:
+				putchar('g'); 
+				break;
+			case 11:
+				putchar('h');
+				break;
+			case 12:
+				putchar('i');
+				break;
+			case 13:
+				putchar('j');
+				break;
+			case 14:
+				putchar('k');
+				break;
+			case 15:
+				putchar('l');
+				break;
+		}
+	}
+	else{
+		char temp = (n%16)+'0';
+		putchar(temp);	
+	}
+}
+}
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'j')) {
-			printf("%s",param);		
+			printf("%s",param);
 			putchar(' ');
-			for(int j = i+1;j<strlen(format_string);j++){
+			int numb = atoi(format_string+(i+2));
+			hex(numb);
 			break;
-			}
 	}else
 			putchar(format_string[i]);
 	}
