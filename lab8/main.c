c317aa539acf268b01e6b21468e2bfc2b690550d
Jakubszadkowski <jakubszadkowski210495@wp.pl>
2023-01-15 14:22:06 +0100

    Before tests

--- lab8/main.c
+++ lab8/main.c
@@ -36,10 +36,25 @@ void hex(long n){
 }
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'j')){
+		if((format_string[i] == '#') && (format_string[i+1] == '.')&&((format_string[i+3] == 'j')||(format_string[i+4] == 'j')))){
 			printf("%s",param);
-			putchar(' ');
-			long numb = atoi(format_string+(i+2));
+			if(!isdigit(format_string[i+5]))
+				break;
+			int atrybut = atoi(format_string+(i+2));
+			
+			int dlugosc = 3;
+			int flag=1;
+			if(format_string[i+3]!=' ')
+				dlugosc++;
+			
+			sputchar(' ');
+			if(atrybut>strlen(format_string)-(j)&&flag==1){
+				flag=2;
+				for(int k = 0;k< atrybut-(strlen(format_string)-j);k++){
+					putchar('0');
+				}
+			}
+			long numb = atoi(format_string+(i+4));
 			hex(numb);
 			break;
 		}else
